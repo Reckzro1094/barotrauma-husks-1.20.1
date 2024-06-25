0d8d49d8aaa5cb9ff5ce382beb6c5c65de36dfa0
Reckzro1094 <bryceklohs@gmail.com>
2024-06-24 19:57:45 -0500

    Initial commit

---
+++ src/main/java/bryce/barotraumahusks/BarotraumaHusksClient.java
@@ -0,0 +1,10 @@
+package bryce.barotraumahusks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BarotraumaHusksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/bryce/barotraumahusks/BarotraumaHusksDataGenerator.java
@@ -0,0 +1,11 @@
+package bryce.barotraumahusks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BarotraumaHusksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
